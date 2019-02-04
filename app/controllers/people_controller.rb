95adb924a3898787fa7db523ace4b463579535d0
George  Pianka <gpianka1@gmail.com>
2019-02-04 09:52:57 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -14,10 +14,21 @@ class PeopleController < ApplicationController
   def index
     @people = Person.all
   end
+  
+  
 
   private
 
-  def person_params
-    params.require(:person).permit(:name)
-  end
-end
+ def person_params
+    params.require(:person).permit(:name)	    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
+  end	  end
